0c4488f503c7ea99ac09ea00088cef5f203f6c03
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2022-05-08 22:08:46 -0400

    First draft of hashipet talk

--- hashicorp-gateway/errors.go
+++
@@ -1,5 +0,0 @@
-func CustomErr(ctx context.Context, mux *runtime.ServeMux, m runtime.Marshaler, w http.ResponseWriter, req *http.Request, err error) {
-	st := status.Convert(err)
-	w.WriteHeader(runtime.HTTPStatusFromCode(st.Code()))
-	w.Write([]byte(st.Message()))
-}
